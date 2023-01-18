13753dd0d5406d9c0e9ec018e847c33bf1516275
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-18 16:16:10 +0100

    added router.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import { router } from './router';
 import App from './App.vue'
 
 import './assets/styles/main.scss'
 
-createApp(App).mount('#app')
+//modo alternativo per scrivere la riga sotto, si equivalgono 
+// const app = createApp(App);
+// app.use(router);
+// app.mounth('#app');
+
+createApp(App).use(router).mount('#app')
---
+++ src/router.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from './pages/HomePage.vue';
+import AbouUsPage from './pages/AboutUsPage.vue';
+import ProjectListPage from './pages/ProjectListPage.vue';
+import SingleProjectPage from './pages/SingleProjectPage.vue';
+import ContactUsPage from './pages/ContactUsPage.vue';
+import NotFound from './pages/NotFound.vue';
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AbouUsPage
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectListPage
+        },
+        {
+            path: '/projects/:slug',
+            name: 'single-project',
+            component: SingleProjectPage
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactUsPage
+        },
+        {
+            path: '/*',
+            name: 'not-found',
+            component: NotFound
+        }
+    ]
+});
+
+export {router};
