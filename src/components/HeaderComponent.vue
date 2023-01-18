eb33735f2e7ff58a0b289c31a46fcf68105b59d4
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-18 16:21:51 +0100

    added header nav links

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,12 @@
 <template>
     <header>
-       header
+        <ul class="d-flex">
+            <li class="ms-5" v-for="(item, index) in menuItems" :key="index">
+                <router-link :to="{name : item.routeName}" active-class="active" class="nav-link">
+                {{ item.label }}
+                </router-link>
+            </li>
+        </ul>
    
     </header>
 </template>
@@ -8,6 +14,29 @@
 <script>
     export default{
     name: 'AppHeader',
+    data(){
+        return{
+            menuItems:[
+                {
+                    label: 'Home',
+                    routeName: 'home'
+                },
+                {
+                    label: 'About',
+                    routeName: 'about'
+                },
+                {
+                    label: 'Portfolio',
+                    routeName: 'projects'
+                },
+                {
+                    label: 'Contacts',
+                    routeName: 'contact'
+                },
+            ]
+        }
+    }
+   
     }
 </script>
 
