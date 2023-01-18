7082e83c1bd178916332b96e6d3756a0ae31ec7b
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-01-18 22:50:12 +0100

    Project show layout wip

--- src/pages/SingleProjectPage.vue
+++ src/pages/SingleProjectPage.vue
@@ -1,15 +1,21 @@
 <template>
     <section v-if="project">
-         <h1>{{project.title}}</h1>
-         <p>{{project.content }}</p>
-         <img :src="`${store.imageBasePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
-         <div v-if="project.category">
+        <div class="show-hero">
+            <div class="my-container">
+                <h2>{{project.title}}</h2>
+                 <p>{{project.content }}</p>
+            </div>
+        </div>
+        <div class="project-img">
+            <img :src="`${store.imageBasePath}${project.cover_image}`" class="card-img-top" :alt="project.title">
+        </div>
+         <div v-if="project.category" class="my-container my-5">
              <h5>Category: {{project.category.name }}</h5>
          </div>
-         <div v-if="project.tags && project.tags.length > 0">
-             <h5>Tags</h5>
+         <div v-if="project.tags && project.tags.length > 0" class="my-container mb-5">
+             <h5 class="mb-3">Tags :</h5>
              <div>
-                 <span v-for="(tag,index) in project.tags" :key="index" class="badge bg-info text-dark">{{tag.name }}</span>
+                 <span v-for="(tag,index) in project.tags" :key="index" class="badge bg-light text-dark me-2">{{tag.name }}</span>
              </div>
          </div>
     </section>
@@ -49,5 +55,29 @@
  </script>
  
  <style lang="scss" scoped>
- 
+ @use '../assets/styles/partials/variables' as *;
+@use '../assets/styles/partials/mixins' as *;
+
+    .show-hero{
+        width: 100%;
+        height: 500px;
+        // background-color: $bg-color-pink;
+        background-image: url(../assets/img/web-dev-05.png);
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+
+        h2{
+            padding-top: 8rem;
+        }
+        p{
+            max-width: 600px;
+            margin-top: 1.5rem;
+        }
+    }
+    .project-img{
+       max-width: 1170px;
+        margin: 0 auto;
+        padding: 0 3rem;
+    }
  </style>
